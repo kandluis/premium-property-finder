0378af98f47d8c267f0329eb0152453146e8fa98
Luis Antonio Perez Martinez <luispe@fb.com>
2020-08-23 00:38:01 -0700

    Allow API-Key

--- server/web.js
+++ server/web.js
@@ -89,7 +89,7 @@ async function refresh() {
 function allowCrossDomains(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,POST');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With Api-Key');
 
   // intercept OPTIONS method
   if ('OPTIONS' == req.method) {
