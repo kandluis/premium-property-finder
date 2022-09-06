f9ae6cae916816638dd047797cb23ca63ab00043
Luis Perez <luis.perez.live@gmail.com>
2022-09-05 19:02:01 -0700

    update to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Home } from './pages/Home';
 import { QueryParamProvider } from 'use-query-params';
+import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6';
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(
-  <Router>
-    <QueryParamProvider ReactRouterRoute={Route}>
-      <Home />
+const rootElement = createRoot(document.getElementById('root')!);
+rootElement.render(
+  <BrowserRouter>
+    <QueryParamProvider adapter={ReactRouter6Adapter}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </QueryParamProvider>
-  </Router>,
-  rootElement,
+  </BrowserRouter>
 );
