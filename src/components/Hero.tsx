0bac057aad0dfeb4875f7c2bfbd5dbb59ca92f1e
Luis Perez <luis.perez.live@gmail.com>
2022-10-23 10:53:05 -0700

    change name and root of page to new url

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ export default function Hero(): React.ReactElement {
   return (
     <div className={classes}>
       <div className="hero-body text-center text-light">
-        <h1>Premium Property Finder</h1>
+        <h1>Homegic</h1>
         <p className="mb-0">Bringing premium property right to your fingertips</p>
       </div>
     </div>
