9c4da74842c191e8deeb04aff8900fb0a68676dc
Luis Perez <luis.perez.live@gmail.com>
2022-11-02 00:32:57 -0700

    improve layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Grid2 from '@mui/material/Unstable_Grid2';
 
 import Filter from '../components/Filter';
 import Hero from '../components/Hero';
@@ -12,25 +13,31 @@ import {
 
 export default function Home(): React.ReactElement {
   return (
-    <>
-      <Hero />
-      <div className="container">
+    <Grid2 container spacing={2}>
+      <Grid2 xs={12}>
+        <Hero />
+      </Grid2>
+      <Grid2 xs={12}>
         <PropertyListingsProvider>
           <PropertyListingsConsumer>
             {({
               loading, percent, filteredProperties, allProperties, remoteUpdate, localUpdate,
             }) => (
               <>
-                <Filter
-                  localUpdate={localUpdate}
-                  remoteUpdate={remoteUpdate}
-                  results={filteredProperties}
-                  all={allProperties}
-                  loading={loading}
-                />
-                {!loading
-                  ? <ResultSummary all={allProperties} filtered={filteredProperties} />
-                  : <ProgressBar value={100 * percent} />}
+                <Grid2 xs={12}>
+                  <Filter
+                    localUpdate={localUpdate}
+                    remoteUpdate={remoteUpdate}
+                    results={filteredProperties}
+                    all={allProperties}
+                    loading={loading}
+                  />
+                </Grid2>
+                <Grid2 xs={12}>
+                  {!loading
+                    ? <ResultSummary all={allProperties} filtered={filteredProperties} />
+                    : <ProgressBar value={100 * percent} />}
+                </Grid2>
                 <div className="columns">
                   {filteredProperties.map((property) => (
                     <Listing property={property} key={`${property.zpid || ''}-${property.statusType}`} />
@@ -40,7 +47,7 @@ export default function Home(): React.ReactElement {
             )}
           </PropertyListingsConsumer>
         </PropertyListingsProvider>
-      </div>
-    </>
+      </Grid2>
+    </Grid2>
   );
 }
